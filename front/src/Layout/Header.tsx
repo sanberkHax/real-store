a2adcc401ac35cc6f11b931aeae53afdcb0b5dab
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2022-10-10 16:52:13 +0300

    Create Header

---
+++ front/src/Layout/Header.tsx
@@ -0,0 +1,18 @@
+import { ReactComponent as CartIcon } from '../assets/cartIcon.svg';
+
+export const Header = () => {
+  return (
+    <header className="w-full h-10 flex justify-between items-center sticky top-0 bg-white">
+      <h1 className="font-bold text-2xl sm:text-3xl text-red-400">
+        Book Store
+      </h1>
+      <button className="flex justify-between gap-8 items-center relative">
+        <h1 className="font-bold text-xl sm:text-2xl">My Cart</h1>
+        <CartIcon />
+        <div className="rounded-full bg-red-400 w-8 h-8 absolute top-5 right-6 flex justify-center items-center">
+          <p className="font-bold">0</p>
+        </div>
+      </button>
+    </header>
+  );
+};
