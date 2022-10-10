012fe62d95488bb388e07b2517f4a1fe45608fc6
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2022-10-10 21:43:04 +0300

    Add test utils

--- front/src/app/store.ts
+++ front/src/app/store.ts
@@ -1,6 +1,24 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  PreloadedState,
+  combineReducers,
+} from '@reduxjs/toolkit';
 import cartReducer from '../slices/cartSlice';
 import booksReducer from '../slices/booksSlice';
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  books: booksReducer,
+});
+
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+}
 
 export const store = configureStore({
   reducer: {
@@ -9,6 +27,7 @@ export const store = configureStore({
   },
 });
 
+export type AppStore = typeof store;
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
