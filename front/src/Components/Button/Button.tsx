d4f2bf0aaa0e01a09b423c12d607dd6dce453c5f
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2022-10-10 16:16:10 +0300

    Add button component

---
+++ front/src/Components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface ButtonProps {
+  text: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+  return (
+    <button
+      className="bg-red-400 rounded lg text-white font-bold px-4 py-2 text-center hover:bg-red-600"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
