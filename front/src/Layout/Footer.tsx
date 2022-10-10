9c94bf1d560aaea282710fc2df6027a00f8baae3
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2022-10-10 16:52:07 +0300

    Create Footer

---
+++ front/src/Layout/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-red-400 font-bold h-8 w-full flex justify-between px-10 justify-self-end items-center">
+      <h2>Book Store</h2>
+      <h2>Sanberk Türker</h2>
+    </footer>
+  );
+};
