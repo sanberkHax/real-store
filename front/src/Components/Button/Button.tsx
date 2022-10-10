6e90b024bdd8137b19561446611e9b17990a5e05
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2022-10-11 02:56:12 +0300

    Update button

--- front/src/Components/Button/Button.tsx
+++ front/src/Components/Button/Button.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 interface ButtonProps {
   text: string;
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
     <button
-      className="bg-red-400 rounded w-full lg text-white font-bold px-4 py-2 text-center hover:bg-red-600"
+      className="bg-red-400 rounded w-full lg text-white font-bold px-4 py-2 text-center hover:bg-red-600 active:scale-90"
       onClick={onClick}
     >
       {text}
