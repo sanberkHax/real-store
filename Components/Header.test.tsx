677caeb068e1083479219383eac623dddf2c68c3
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-03-18 00:48:45 +0300

    Refactor header test

--- Components/Header.test.tsx
+++ Components/Header.test.tsx
@@ -1,30 +1,45 @@
 import { screen } from '@testing-library/react';
 import { renderWithProviders } from '../utils/test-utils';
-import { Header } from '@/Components/Header';
+import mockRouter from 'next-router-mock';
+import { CartItem } from '@/redux/slices/cartSlice';
+
+const DUMMY_CART: CartItem[] = [
+  {
+    id: 1,
+    title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    price: 109.95,
+    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    quantity: 1,
+  },
+];
 
 describe('Header', () => {
-  it('should render correct cart item numbers', async () => {
-    renderWithProviders(<Header />, {
+  it('should render My Cart button if the cart is empty', () => {
+    mockRouter.push('/cart');
+
+    renderWithProviders(<></>, {
       preloadedState: {
         cart: {
-          cart: [
-            {
-              id: 0,
-              title: 'string',
-              author: 'string',
-              cover_url: 'string',
-              pages: 0,
-              price: 0,
-              currency: 'string',
-              quantity: 4,
-            },
-          ],
-          status: 'idle',
-          totalQuantity: 4,
+          cart: [],
+          totalQuantity: 0,
+          totalPrice: 0,
         },
-        books: { books: [], status: 'idle' },
       },
     });
-    expect(screen.getByText(/4/i)).toBeInTheDocument();
+    expect(screen.getByText(/My Cart/i)).toBeInTheDocument();
+  });
+  it('should render checkout button if there is a product in cart', () => {
+    mockRouter.push('/cart');
+
+    renderWithProviders(<></>, {
+      preloadedState: {
+        cart: {
+          cart: DUMMY_CART,
+          totalQuantity: 1,
+          totalPrice: 100,
+        },
+      },
+    });
+    expect(screen.getByText(/Checkout/i)).toBeInTheDocument();
   });
 });
