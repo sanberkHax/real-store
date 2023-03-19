cadd0354ae71dd524f2ebc4a2102d160481dac99
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-03-19 17:43:08 +0300

    Add footer

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Header } from './Header';
 
 export const Layout = ({ children }) => {
@@ -5,6 +6,17 @@ export const Layout = ({ children }) => {
     <div className="flex flex-col h-screen gap-10">
       <Header />
       {children}
+      <footer className="p-4 text-center bg-orange-500 text-white">
+        <p>
+          Designed & Built by
+          <Link
+            href="https://www.sanberk.dev"
+            className="font-bold text-black hover:text-slate-600"
+          >
+            &nbsp;Sanberk Türker
+          </Link>
+        </p>
+      </footer>
     </div>
   );
 };
