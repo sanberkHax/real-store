f38d064ef1bc987f58928a1e11270113c098205c
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-03-19 16:11:39 +0300

    Update header title

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -29,7 +29,7 @@ export const Header = () => {
 
   return (
     <header className="w-full h-10 flex justify-between items-center fixed bg-white px-2 sm:px-4 md:px-8 lg:px-10 py-8 z-10">
-      <Link href="/" className="font-bold text-2xl sm:text-3xl text-red-400">
+      <Link href="/" className="font-bold text-2xl sm:text-3xl">
         Real Store
       </Link>
       {router.pathname === '/cart' && cart.length !== 0 ? (
