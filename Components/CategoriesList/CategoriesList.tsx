aa217936c334c3cb9a5c884df671241df25424ae
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-04-11 15:27:50 +0300

    Fix link

--- Components/CategoriesList/CategoriesList.tsx
+++ Components/CategoriesList/CategoriesList.tsx
@@ -9,7 +9,13 @@ export const CategoriesList = ({ categories }: Props) => {
     <ul className="flex bg-sky-600 justify-around items-center text-white font-bold text-md sm:text-xl text-center flex-wrap sm:flex-nowrap">
       {categories.map((c, i) => (
         <li key={i} className="flex items-center justify-center">
-          <Link href="/products/electronics" className="hover:bg-sky-500 p-4">
+          <Link
+            href={`/products/${c
+              .replace("'", '')
+              .replace(/\s+/g, '-')
+              .toLowerCase()}`}
+            className="hover:bg-sky-500 p-4"
+          >
             {c}
           </Link>
         </li>
