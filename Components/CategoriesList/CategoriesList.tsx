ad1b427d8c26823c93b0a28fcf5dc8cbfb337bd7
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-04-11 15:30:14 +0300

    Fix dark mode

--- Components/CategoriesList/CategoriesList.tsx
+++ Components/CategoriesList/CategoriesList.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const CategoriesList = ({ categories }: Props) => {
   return (
-    <ul className="flex bg-sky-600 justify-around items-center text-white font-bold text-md sm:text-xl text-center flex-wrap sm:flex-nowrap">
+    <ul className="flex dark:bg-sky-600 bg-orange-500 justify-around items-center text-white font-bold text-md sm:text-xl text-center flex-wrap sm:flex-nowrap">
       {categories.map((c, i) => (
         <li key={i} className="flex items-center justify-center">
           <Link
@@ -14,7 +14,7 @@ export const CategoriesList = ({ categories }: Props) => {
               .replace("'", '')
               .replace(/\s+/g, '-')
               .toLowerCase()}`}
-            className="hover:bg-sky-500 p-4"
+            className="dark:hover:bg-sky-500 hover:bg-orange-600 p-4"
           >
             {c}
           </Link>
